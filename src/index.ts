1dd92ece9e2add4acd6b9ddf4309c8594a7ab4f0
André Reis <arrr.cin@gmail.com>
2022-07-08 12:06:21 -0400

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello world!')
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: any, res: any) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at https://localhost:${port}`);
+});
